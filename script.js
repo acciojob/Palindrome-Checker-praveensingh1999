e86729c0f6aabcd4751a8b62db455cc6832f040b
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-11-10 19:38:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+ let first =0;
+	let last =str.length;
+	while(first<=last){
+		if(str[first]!=str[last]){
+			return false;
+		}
+		first++;
+		last--;
+	}
+	return true;
 }
 module.exports = palindrome
